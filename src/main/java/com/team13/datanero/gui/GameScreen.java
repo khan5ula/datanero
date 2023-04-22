d4c37a5bdc96fe9a21765020b7eb7ecedbf4e808
Kristian Hannula <kristian.hannula@student.oulu.fi>
2023-04-22 20:33:39 +0300

    Positioned game screen content better

--- src/main/java/com/team13/datanero/gui/GameScreen.java
+++ src/main/java/com/team13/datanero/gui/GameScreen.java
@@ -7,14 +7,17 @@ import com.team13.datanero.backend.Game;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+
 
 public class GameScreen extends JPanel {
     private MainFrame mainFrame;
     private Game game;
-    private JLabel questionLabel;
     private JButton[] answerButtons;
     private JLabel scoreLabel;
     private JLabel livesLabel;
+    private JTextArea questionTextArea;
 
     public GameScreen(MainFrame mainFrame, Game game) {
         this.mainFrame = mainFrame;
@@ -24,9 +27,20 @@ public class GameScreen extends JPanel {
 
         Insets padding = new Insets(5, 5, 5, 5);
 
+        // Set an empty border with a 20-pixel padding around the panel
+        Border borderPadding = BorderFactory.createEmptyBorder(20, 20, 20, 20);
+        setBorder(borderPadding);
+
         // Create and configure components
-        questionLabel = new JLabel(game.getCurrentQuestion());
-        questionLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        questionTextArea = new JTextArea(game.getCurrentQuestion());
+        questionTextArea.setFont(new Font("Arial", Font.BOLD, 48));
+        questionTextArea.setLineWrap(true);
+        questionTextArea.setWrapStyleWord(true);
+        questionTextArea.setEditable(false);
+        questionTextArea.setBackground(null);
+        questionTextArea.setOpaque(false);
+        questionTextArea.setFocusable(false);
+        
 
         answerButtons = new JButton[4];
         String[] answers = game.getAnswersForCurrentQuestion();
@@ -36,10 +50,10 @@ public class GameScreen extends JPanel {
         }
 
         scoreLabel = new JLabel("Pisteet: " + game.getScore());
-        scoreLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        scoreLabel.setFont(new Font("Arial", Font.BOLD, 32));
 
         livesLabel = new JLabel("Elämät: " + game.getLives());
-        livesLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        livesLabel.setFont(new Font("Arial", Font.BOLD, 32));
 
         GridBagConstraints gbc = new GridBagConstraints();
 
@@ -62,32 +76,36 @@ public class GameScreen extends JPanel {
         scoreAndLivesPanel.add(livesLabel, scoreAndLivesPanelConstraints);
 
         // Add the score and lives panel
-        gbc.gridx = 1;
+        gbc.gridx = 10;
         gbc.gridy = 0;
-        gbc.gridheight = 2;
-        gbc.anchor = GridBagConstraints.NORTHEAST;
+        gbc.gridwidth = 2; // Span 2 columns
+        gbc.gridheight = 2; // Span 2 rows
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbc.fill = GridBagConstraints.BOTH; // Make the component fill the grid cell
         gbc.insets = padding;
         add(scoreAndLivesPanel, gbc);
-
-
-        // Add question label
+        
+        /* Add the question label to the grid */
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.gridwidth = 2;
+        gbc.gridwidth = 9; // Span the entire width
+        gbc.gridheight = 3; // Occupy the top 4 rows
         gbc.weightx = 1.0;
-        gbc.weighty = 0.34;
+        gbc.weighty = 0.5;
         gbc.anchor = GridBagConstraints.NORTH;
+        gbc.fill = GridBagConstraints.BOTH; // Make the component fill the grid cell
         gbc.insets = padding;
-        add(questionLabel, gbc);
+        add(questionTextArea, gbc);
 
         // Add answer buttons
-        gbc.gridwidth = 1;
+        gbc.gridwidth = 3; // Each button occupies 3 columns
+        gbc.gridheight = 2; // Each button occupies 2 rows
         gbc.anchor = GridBagConstraints.CENTER;
-        gbc.weighty = 0.66;
+        gbc.fill = GridBagConstraints.BOTH; // Make the component fill the grid cell
         gbc.insets = new Insets(2, 5, 2, 5); // Reduced vertical space
         for (int i = 0; i < answerButtons.length; i++) {
-            gbc.gridx = i % 2;
-            gbc.gridy = 1 + i / 2;
+            gbc.gridx = 3 + (i % 2) * 3; // Calculate the gridx value for each button
+            gbc.gridy = 4 + (i / 2) * 2; // Place the buttons below the question
             add(answerButtons[i], gbc);
         }
 
@@ -98,14 +116,17 @@ public class GameScreen extends JPanel {
         ButtonActions buttonActions = new ButtonActions(this.mainFrame);
         lopetaButton.addActionListener(buttonActions);
 
-        gbc.gridx = 0;
-        gbc.gridy = 4;
-        gbc.gridwidth = 2;
+        gbc.gridx = 10;
+        gbc.gridy = 8;
+        gbc.gridwidth = 2; // Span 2 columns
+        gbc.gridheight = 2; // Span 2 rows
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
         gbc.anchor = GridBagConstraints.SOUTHEAST;
+        gbc.fill = GridBagConstraints.NONE; // Don't make the component fill the grid cell
         gbc.insets = padding;
         add(lopetaButton, gbc);
+        
 
         // Load the mascot image
         ImageIcon mascotIcon = new ImageIcon("src/main/java/com/team13/datanero/images/mascot.png");
@@ -115,19 +136,18 @@ public class GameScreen extends JPanel {
         mascotLabel.setIcon(mascotIcon);
 
         // Add the JLabel to the GameScreen panel
-        gbc.gridx = 2;
-        gbc.gridy = 0;
-        gbc.gridheight = GridBagConstraints.REMAINDER; // Set gridheight to span all remaining rows
-        gbc.anchor = GridBagConstraints.CENTER; // Center the mascot image vertically
+        gbc.gridx = 9; // Adjust the x-coordinate as needed
+        gbc.gridy = 2; // Adjust the y-coordinate as needed
+        gbc.gridwidth = 3; // You can adjust the width as needed
+        gbc.gridheight = 10; // You can adjust the height as needed
+        gbc.anchor = GridBagConstraints.CENTER; // Center the mascot image
         gbc.insets = padding;
         add(mascotLabel, gbc);
-
-
     }
 
     private void updateGameDisplay() {
         String[] answers = game.getAnswersForCurrentQuestion();
-        questionLabel.setText(game.getCurrentQuestion());
+        questionTextArea.setText(game.getCurrentQuestion());
         for (int i = 0; i < answerButtons.length; i++) {
             answerButtons[i].setText(answers[i]);
         }
