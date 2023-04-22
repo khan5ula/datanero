8c8f0d895edccdf848961e8c8e9b7ac6fc01bb64
Kristian Hannula <kristian.hannula@student.oulu.fi>
2023-04-22 16:10:55 +0300

    Added mascot to game screen

--- src/main/java/com/team13/datanero/gui/GameScreen.java
+++ src/main/java/com/team13/datanero/gui/GameScreen.java
@@ -22,6 +22,8 @@ public class GameScreen extends JPanel {
 
         setLayout(new GridBagLayout());
 
+        Insets padding = new Insets(5, 5, 5, 5);
+
         // Create and configure components
         questionLabel = new JLabel(game.getCurrentQuestion());
         questionLabel.setFont(new Font("Arial", Font.BOLD, 24));
@@ -41,35 +43,53 @@ public class GameScreen extends JPanel {
 
         GridBagConstraints gbc = new GridBagConstraints();
 
+        // Create score and lives panel
+        JPanel scoreAndLivesPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints scoreAndLivesPanelConstraints = new GridBagConstraints();
+
+        // Add score label
+        scoreAndLivesPanelConstraints.gridx = 0;
+        scoreAndLivesPanelConstraints.gridy = 0;
+        scoreAndLivesPanelConstraints.anchor = GridBagConstraints.NORTHWEST;
+        scoreAndLivesPanelConstraints.insets = padding;
+        scoreAndLivesPanel.add(scoreLabel, scoreAndLivesPanelConstraints);
+
+        // Add lives label
+        scoreAndLivesPanelConstraints.gridx = 0;
+        scoreAndLivesPanelConstraints.gridy = 1;
+        scoreAndLivesPanelConstraints.anchor = GridBagConstraints.NORTHWEST;
+        scoreAndLivesPanelConstraints.insets = padding;
+        scoreAndLivesPanel.add(livesLabel, scoreAndLivesPanelConstraints);
+
+        // Add the score and lives panel
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridheight = 2;
+        gbc.anchor = GridBagConstraints.NORTHEAST;
+        gbc.insets = padding;
+        add(scoreAndLivesPanel, gbc);
+
+
         // Add question label
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 2;
         gbc.weightx = 1.0;
-        gbc.weighty = 1.0;
+        gbc.weighty = 0.34;
         gbc.anchor = GridBagConstraints.NORTH;
-        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.insets = padding;
         add(questionLabel, gbc);
 
         // Add answer buttons
         gbc.gridwidth = 1;
         gbc.anchor = GridBagConstraints.CENTER;
+        gbc.weighty = 0.66;
+        gbc.insets = new Insets(2, 5, 2, 5); // Reduced vertical space
         for (int i = 0; i < answerButtons.length; i++) {
             gbc.gridx = i % 2;
             gbc.gridy = 1 + i / 2;
             add(answerButtons[i], gbc);
         }
-
-        // Add score label
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        gbc.anchor = GridBagConstraints.WEST;
-        add(scoreLabel, gbc);
-
-        // Add lives label
-        gbc.gridx = 1;
-        gbc.anchor = GridBagConstraints.EAST;
-        add(livesLabel, gbc);
 
         // Add exit button
         JButton lopetaButton = new ButtonFactory("Lopeta", 'L', new Color(239, 71, 111)).getButton();
@@ -84,8 +104,25 @@ public class GameScreen extends JPanel {
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
         gbc.anchor = GridBagConstraints.SOUTHEAST;
-        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.insets = padding;
         add(lopetaButton, gbc);
+
+        // Load the mascot image
+        ImageIcon mascotIcon = new ImageIcon("src/main/java/com/team13/datanero/images/mascot.png");
+
+        // Create a JLabel to hold the image and set the icon
+        JLabel mascotLabel = new JLabel();
+        mascotLabel.setIcon(mascotIcon);
+
+        // Add the JLabel to the GameScreen panel
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        gbc.gridheight = GridBagConstraints.REMAINDER; // Set gridheight to span all remaining rows
+        gbc.anchor = GridBagConstraints.CENTER; // Center the mascot image vertically
+        gbc.insets = padding;
+        add(mascotLabel, gbc);
+
+
     }
 
     private void updateGameDisplay() {
